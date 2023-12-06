49a55f128affe9103198015448f6ff3ed67579f4
filippo199600 <pippotartaglia@gmail.com>
2023-12-06 23:38:36 +0100

    add table pivot order_dish

---
+++ database/migrations/2023_12_05_074843_create_orders_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('restaurant_id')->constrained();
+            $table->string('name', 50);
+            $table->string('lastname', 50);
+            $table->string('email');
+            $table->string('phone', 20);
+            $table->string('address');
+            $table->string('address_number');
+            $table->decimal('total');
+            $table->boolean('success');
+            $table->dateTime('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign(['restaurant_id']);
+            $table->dropColumn('restaurant_id');
+        });
+
+        Schema::dropIfExists('orders');
+    }
+};
